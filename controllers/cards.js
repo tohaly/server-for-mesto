40ebf4582dbdc48ea5207ca459d2e91a22b371df
Anton Voitenko <voytenko.ad@yandex.ru>
2020-02-02 15:49:44 +0800

    Adde opportunity like and dislike for card

--- controllers/cards.js
+++ controllers/cards.js
@@ -5,17 +5,27 @@ module.exports.getCards = (req, res) => {
     .then(card => res.send({ data: card }))
     .catch(err => res.status(500).send({ message: `Произошла ошибка: ${err}` }));
 };
-
 module.exports.createCard = (req, res) => {
-  const { name, link, owner } = req.body;
+  const { name, link } = req.body;
 
   Card.create({ name, link, owner: req.user._id })
     .then(card => res.send({ data: card }))
     .catch(err => res.status(500).send({ message: `Произошла ошибка: ${err}` }));
 };
-
 module.exports.deleteCardById = (req, res) => {
   Card.findByIdAndRemove(req.params.cardId)
     .then(() => res.send({ message: `Карточка успешно удалена!` }))
+    .catch(err => res.status(500).send({ message: `Произошла ошибка: ${err}` }));
+};
+module.exports.likeCard = (req, res) => {
+  Card.findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: req.user._id } }, { new: true })
+    .populate('user')
+    .then(card => res.send({ data: card }))
+    .catch(err => res.status(500).send({ message: `Произошла ошибка: ${err}` }));
+};
+module.exports.dislikeCard = (req, res) => {
+  Card.findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } }, { new: true })
+    .populate('user')
+    .then(card => res.send({ data: card }))
     .catch(err => res.status(500).send({ message: `Произошла ошибка: ${err}` }));
 };
