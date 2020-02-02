de132106a83727c85acb473e8cb31d050c4e6112
Anton Voitenko <voytenko.ad@yandex.ru>
2020-02-02 15:27:52 +0800

    Adde opportunity chege user-info and avatar

--- controllers/users.js
+++ controllers/users.js
@@ -2,17 +2,29 @@ const User = require('../models/user');
 
 module.exports.getUsers = (req, res) => {
   User.find({})
-    .then(users => res.send({ data: users }))
+    .then(user => res.send({ data: user }))
     .catch(err => res.status(500).send({ message: `Произошла ошибка: ${err}` }));
 };
 module.exports.getUserById = (req, res) => {
   User.findById(req.params.userId)
-    .then(users => res.send({ data: users }))
+    .then(user => res.send({ data: user }))
     .catch(err => res.status(500).send({ message: `Произошла ошибка: ${err}` }));
 };
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
-    .then(users => res.send({ data: users }))
+    .then(user => res.send({ data: user }))
+    .catch(err => res.status(500).send({ message: `Произошла ошибка: ${err}` }));
+};
+module.exports.updateProfile = (req, res) => {
+  const { name, about } = req.body;
+  User.findByIdAndUpdate(req.user._id, { name, about })
+    .then(() => res.send({ name, about }))
+    .catch(err => res.status(500).send({ message: `Произошла ошибка: ${err}` }));
+};
+module.exports.updateAvatar = (req, res) => {
+  const { avatar } = req.body;
+  User.findByIdAndUpdate(req.user._id, { avatar })
+    .then(() => res.send({ avatar }))
     .catch(err => res.status(500).send({ message: `Произошла ошибка: ${err}` }));
 };
--- routers/users.js
+++ routers/users.js
@@ -1,10 +1,20 @@
 const router = require('express').Router();
-const { getUsers, getUserById, createUser } = require('../controllers/users');
+const {
+  getUsers,
+  getUserById,
+  createUser,
+  updateProfile,
+  updateAvatar
+} = require('../controllers/users');
 
 router.get('/', getUsers);
 
 router.get('/:userId', getUserById);
 
 router.post('/', createUser);
+
+router.patch('/me', updateProfile);
+
+router.patch('/me/avatar', updateAvatar);
 
 module.exports = router;
