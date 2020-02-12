cdc0cf3e92a74cb1f4d374cad4f330cec2610245
Anton Voitenko <voytenko.ad@yandex.ru>
2020-02-12 20:29:02 +0800

    Added email and password in user model

--- models/user.js
+++ models/user.js
@@ -1,32 +1,49 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
-const { validOPtion } = require('../libs/validOption');
+const { validOption } = require('../libs/validOption');
 
 const UserSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      required: validOPtion.requiredField,
-      minlength: validOPtion.minChar,
-      maxlength: validOPtion.maxChar
+      required: validOption.requiredField,
+      minlength: validOption.minChar,
+      maxlength: validOption.maxChar
     },
     about: {
       type: String,
-      required: validOPtion.requiredField,
-      minlength: validOPtion.minChar,
-      maxlength: validOPtion.maxChar
+      required: validOption.requiredField,
+      minlength: validOption.minChar,
+      maxlength: validOption.maxChar
     },
     avatar: {
       type: String,
-      required: validOPtion.requiredField,
+      required: validOption.requiredField,
       validate: {
         validator(valid) {
           return validator.isURL(valid);
         },
-        message: props => `${props.value} ${validOPtion.urlMessage}`
+        message: props => `${props.value} ${validOption.urlMessage}`
       }
+    },
+    email: {
+      type: String,
+      required: validOption.requiredField,
+      unique: true,
+      validate: {
+        validator(valid) {
+          return validator.isEmail(valid);
+        },
+        message: props => `${props.value} ${validOption.emailMessage}`
+      }
+    },
+    password: {
+      type: String,
+      required: validOption.requiredField,
+      minlength: validOption.minPasswordLength
     }
   },
+
   {
     versionKey: false
   }
