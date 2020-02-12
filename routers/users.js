7b1f48bedb8b2bab2cd2e8073ea55b22d30b46c6
Anton Voitenko <voytenko.ad@yandex.ru>
2020-02-12 22:52:54 +0800

    Deleted 'createUsers' router

--- routers/users.js
+++ routers/users.js
@@ -1,16 +1,9 @@
 const router = require('express').Router();
-const {
-  getUsers,
-  getUserById,
-  createUser,
-  updateProfile,
-  updateAvatar
-} = require('../controllers/users');
+const { getUsers, getUserById, updateProfile, updateAvatar } = require('../controllers/users');
 const { doesUserExist } = require('../middlewares/doesUserExist');
 
 router.get('/', getUsers);
 router.get('/:userId', doesUserExist, getUserById);
-router.post('/', createUser);
 router.patch('/me', updateProfile);
 router.patch('/me/avatar', updateAvatar);
 
