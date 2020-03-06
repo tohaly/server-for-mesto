e4225f9c4d38af634c6c1f9eb87f4ffcbf326cce
Anton Voitenko <voytenko.ad@yandex.ru>
2020-03-06 19:26:01 +0800

    Litner errors fixed

--- errors/error-handler.js
+++ errors/error-handler.js
@@ -1,9 +1,10 @@
 const translateErrors = require('./translate-errors');
 
+// eslint-disable-next-line no-unused-vars
 module.exports = (err, req, res, next) => {
   if (!err.statusCode) {
     return translateErrors(err, res);
   }
 
-  res.status(err.statusCode).send({ message: err.message });
+  return res.status(err.statusCode).send({ message: err.message });
 };
